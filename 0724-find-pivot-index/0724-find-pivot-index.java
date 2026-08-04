ed8e980f81fe34dba9b6c03edbcfab64e4ceaf1b
sneha1972-jha <snehajha1972@gmail.com>
2026-08-05 00:15:58 +0530

    Time: 1 ms (97.45%), Space: 47.3 MB (78.89%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int n = nums.length;
+
+       int total=0;
+       for(int num:nums){
+        total+=num;
+       }
+       int left=0;
+       for(int i=0;i<n;i++){
+        int right=total-left-nums[i];
+        if(right==left){
+            return i;
+        }
+        left+=nums[i];
+       }
+        return -1;
+    }
+}
