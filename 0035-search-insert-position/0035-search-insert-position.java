38025f406b4240cffe1cf5572c5392fff917e02b
sneha1972-jha <snehajha1972@gmail.com>
2026-08-14 21:13:18 +0530

    Time: 0 ms (100%), Space: 44.7 MB (48.73%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int n=nums.length;
+        int left=0;
+        int right=n-1;
+       
+        while(left<=right){
+            int mid=left+((right-left)/2);
+            if(target==nums[mid]){
+                
+                return mid;
+            }
+            if(target<nums[mid]){
+                right=mid-1;
+            }
+            if(target>nums[mid]){
+                left=mid+1;
+            }
+        }
+        return left;
+    }
+}
